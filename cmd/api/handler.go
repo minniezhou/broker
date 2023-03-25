4c81a67244db62afd3202eb6cc017d708841cc2c
Min Zhou <min.zhou@salesforce.com>
2023-03-24 21:13:44 -0400

    first-commit

---
+++ cmd/api/handler.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+type Handler struct {
+	router *chi.Mux
+}
+
+func (c *Config) Newhandler() *Handler {
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http//*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Use(middleware.Logger)
+	r.Post("/", c.broker)
+	r.Get("/hello", c.getHello)
+	return &Handler{
+		router: r,
+	}
+}
+
+func (c *Config) getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (c *Config) broker(w http.ResponseWriter, r *http.Request) {
+	response := jsonResponse{
+		Error:   false,
+		Message: "hit the broken service",
+	}
+	output, _ := json.MarshalIndent(response, "", "  ")
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(output)
+}
---
+++ cmd/api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Config struct{}
+
+func main() {
+	c := Config{}
+	h := c.Newhandler()
+	log.Println("server started at port 8080...")
+	err := http.ListenAndServe(":8080", h.router)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server %s \n", err)
+		os.Exit(1)
+	}
+}
